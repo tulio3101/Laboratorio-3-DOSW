0b9f0c2d96fbff84d9a015101381ca90ec4bb91c
curso curso <curso@labinfo.is.escuelaing.edu.co>
2025-08-29 15:41:34 -0500

    Parte 2 -Reto#3 : Julian Lopez -Fibonacci y clase principal

---
+++ src/main/java/edu/dosw/lab/Reto3/PlanningPoker.java
@@ -0,0 +1,14 @@
+package edu.dosw.lab.Reto3;
+import java.util.*;
+public class PlanningPoker {
+    List<String> tareas = new ArrayList<>(); 
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el numero de personas que componen el grupo: ");
+        int numeroPersonas = scanner.nextInt();
+        System.out.println("Ingrese el numero de tareas por realizar: ");
+        int numeroTareas = scanner.nextInt();
+        
+    }
+    
+}
