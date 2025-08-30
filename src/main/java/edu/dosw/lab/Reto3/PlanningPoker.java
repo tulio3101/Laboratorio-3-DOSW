6e850b5c6f259f0cdf2a97915a51ea74efbe3add
Tulio <tuliojr3101@gmail.com>
2025-08-29 21:11:02 -0500

    Parte 2 - Reto#3: Tulio - Correcion metodo

--- src/main/java/edu/dosw/lab/Reto3/PlanningPoker.java
+++ src/main/java/edu/dosw/lab/Reto3/PlanningPoker.java
@@ -1,38 +1,41 @@
 package edu.dosw.lab.Reto3;
+
 import java.util.*;
+
 public class PlanningPoker {
     static List<String> tareas = new ArrayList<>();
     static List<Integer> votos = new ArrayList<>();
-    public static void ejecutar(){
+
+    public static void ejecutar() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese el numero de personas que componen el grupo: ");
         int numeroPersonas = scanner.nextInt();
         scanner.nextLine();
         System.out.println("Ingrese el numero de tareas por realizar: ");
         int numeroTareas = scanner.nextInt();
-        for (int i = 0; i < numeroTareas; i++){
+        for (int i = 0; i < numeroTareas; i++) {
             System.out.println("Tarea " + (i + 1) + ": ");
             String entrada = scanner.nextLine();
             tareas.add(entrada);
         }
         menu();
-        while (true){
-            for (int i = 0; i < tareas.size(); i++){
+        while (true) {
+            for (int i = 0; i < tareas.size(); i++) {
                 System.out.println("Votacion Tarea " + (i + 1));
-                for (int j = 0; i < numeroPersonas; i++){
+                for (int j = 0; i < numeroPersonas; i++) {
                     int votacion = scanner.nextInt();
                     votos.add(votacion);
                 }
-                Fibonacci.validateVotesFibonacci(numeroPersonas);
+                ValidateVotes.validateVotesFibonacci(numeroPersonas, votos);
             }
         }
-        
+
     }
 
-    public static void menu(){
-        for (int i = 0; i < tareas.size(); i++){
-            System.out.println((i+1) + "." + tareas.get(i));
+    public static void menu() {
+        for (int i = 0; i < tareas.size(); i++) {
+            System.out.println((i + 1) + "." + tareas.get(i));
         }
     }
-    
+
 }
--- src/main/java/edu/dosw/lab/Reto3/ValidateVotes.java
+++ src/main/java/edu/dosw/lab/Reto3/ValidateVotes.java
@@ -1,12 +1,13 @@
 package edu.dosw.lab.Reto3;
+
 import java.util.*;
 
 public class ValidateVotes {
-    List<Integer> votes = new ArrayList<>();
-    private Fibonacci fibonacci;
-    public boolean validateVotesFibonacci(int n){
-        for(int vote : votes){
-            if(vote ==  fibonacci.calculateFibonacci(n)){
+    private static Fibonacci fibonacci;
+
+    public static boolean validateVotesFibonacci(int n, List<Integer> votes) {
+        for (int vote : votes) {
+            if (vote == fibonacci.calculateFibonacci(n)) {
                 return true;
             }
         }
